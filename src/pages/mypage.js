ac739309a4036acbea3edd2097718c7f73b8f74b
curist <curist.cyc@gmail.com>
2020-05-20 19:07:29 +0800

    another form

--- src/pages/mypage.js
+++ src/pages/mypage.js
@@ -2,5 +2,28 @@ import Template from '../components/Template.js'
 
 export default () => [Template, [
   ['h1', 'My Second Page'],
-  'and some content'
+  'and some content',
+  ['form', {
+    onsubmit: function(e, el) {
+      e.preventDefault()
+      const $input = el.querySelector('[name=code]')
+      const $message = el.querySelector('.message')
+      $message.innerHTML = $input.value
+
+      clearTimeout(el.dataset.timeout)
+      el.dataset.timeout = setTimeout(function() {
+        $message.innerHTML = ''
+      }, 3000)
+    },
+  }, [
+    ['style', {
+      onload: (e, el) => console.log(e, el.parentNode),
+    }],
+    ['input', {
+      name: 'code',
+      placeholder: 'code',
+    }],
+    ['input', { type: 'submit' }],
+    ['div', { class: 'message' }],
+  ]],
 ]]
